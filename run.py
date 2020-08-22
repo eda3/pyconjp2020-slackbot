b6f829922359cbe6e492d4f1947d773e87b8b46b
Shigeo Maruse <maruse.shigeo@isfnet.com>
2020-08-22 14:18:40 +0000

    Successful deployment on App Engine.

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 
-from flask import Flask
 from slackbot.bot import Bot
 
-app = Flask(__name__)
-
-@app.route('/', methods=['POST'])
 def main():
     bot = Bot()
     bot.run()
+
+app = main()
 
 if __name__ == "__main__":
     print('Running slackbot...')
