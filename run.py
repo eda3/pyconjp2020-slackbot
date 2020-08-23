b62cc93352883501237b9bcc858dfeed26a76e37
Shigeo Maruse <maruse.shigeo@isfnet.com>
2020-08-23 14:06:56 +0900

    Foreground execution for simplicity.

--- run.py
+++ run.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
 
-import sched, time
 from slackbot.bot import Bot
-
-s = sched.scheduler(time.time, time.sleep)
 
 def app():
     bot = Bot()
     bot.run()
 
-s.enter(10, 1, app)
+app()
