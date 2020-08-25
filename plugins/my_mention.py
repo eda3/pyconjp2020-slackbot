a9e724b1cd0c4880cd4b91e00a9c080d9ddc5fd9
Shigeo Maruse <maruse.shigeo@isfnet.com>
2020-08-26 01:02:48 +0900

    Who and what in English.

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -3,6 +3,7 @@
 
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
+from datetime import datetime, date, time, timedelta
 import re
 import tempfile
 
@@ -21,6 +22,32 @@ with tempfile.NamedTemporaryFile(delete=True) as t:
     with open(t.name, 'r') as f:
         data = pd.read_csv(f)
 
+data['start_day'] = pd.to_datetime(data['day']).apply(lambda s: s.date())
+data['start_time'] = data['no'].apply(time.fromisoformat)
+z = zip(data['start_day'].to_list(), data['start_time'].to_list())
+data['start'] = [datetime.combine(d, t) for d, t in z]
+data['end'] = data['start'] + pd.to_timedelta(data['length_minutes'], unit='minutes')
+
+@listen_to('who', re.IGNORECASE)
+def get_speaker(message):
+    t = datetime.now()
+    # t = datetime(2020, 8, 28, 14, 0)
+    on_stage = data[(data['start'] <= t) & (data['end'] >= t)]['name']
+    if len(on_stage) == 0:
+        message.reply("It's {:%H:%M} now, but, sorry, nobody on air.".format(t))
+    else:
+        message.reply("It's {:%H:%M} now!  Who's on stage?\n{}".format(t, '\n'.join(on_stage.to_list())))
+
+@listen_to('what', re.IGNORECASE)
+def get_title(message):
+    # t = datetime.now()
+    t = datetime(2020, 8, 28, 14, 0)
+    on_air= data[(data['start'] <= t) & (data['end'] >= t)]['title']
+    if len(on_air) == 0:
+        message.reply("It's {:%H:%M} now, but, sorry, nothing on air.".format(t))
+    else:
+        message.reply("It's {:%H:%M} now!  What's on air?\n{}".format(t, '\n'.join(on_air.to_list())))
+
 @respond_to('hi', re.IGNORECASE)
 def hi(message):
     message.reply('I can understand hi or HI!')
@@ -28,7 +55,7 @@ def hi(message):
     message.react('+1')
 
 @respond_to('こんにちは')
-def hi(message):
+def hi_jp(message):
     message.reply('こんにちは！')
     # react with thumb up emoji
     message.react('+1')
@@ -38,7 +65,7 @@ def love(message):
     message.reply('I love you too!')
 
 @respond_to('かわいい|愛している')
-def love(message):
+def love_jp(message):
     message.reply('愛情を感じるにゃん')
 
 @listen_to('Can someone help me?')
@@ -53,7 +80,7 @@ def help(message):
     message.reply("Here's a threaded reply", in_thread=True)
 
 @listen_to('ヘルプ')
-def help(message):
+def help_jp(message):
     # Message is replied to the sender (prefixed with @user)
     message.reply('お任せあれ！')
 
